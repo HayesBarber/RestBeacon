9653c5e6365e3a16f6ae67305e9800b4f952104b
Hayes Barber <barberhayes@gmail.com>
2025-07-10 08:27:20 -0400

    add discovery passphrase

--- src/RestBeacon.h
+++ src/RestBeacon.h
@@ -2,6 +2,7 @@
 #define REST_BEACON_H
 
 #include "Message.h"
+#include <Arduino.h>
 #include <WebServer.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -25,7 +26,7 @@ public:
      * @param httpPort The HTTP port to listen on for POST messages.
      * @param udpPort The UDP port to listen on for discovery broadcasts.
      */
-    RestBeacon(uint16_t httpPort = 80, uint16_t udpPort = 4210);
+    RestBeacon(uint16_t httpPort = 80, uint16_t udpPort = 4210, String discoveryPassphrase = "WHO_IS_THERE");
 
     /**
      * @brief Starts the HTTP server and UDP listener.
@@ -60,6 +61,7 @@ private:
     WiFiUDP _udp;
     MessageCallback _messageCallback;
     DiscoveryCallback _discoveryCallback;
+    String _discoveryPassphrase;
 
     uint16_t _udpPort;
     uint16_t _httpPort;
