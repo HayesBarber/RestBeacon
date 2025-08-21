dfe78650b74a1b7223ce7f5268790798c3389e1f
Hayes Barber <barberhayes@gmail.com>
2025-08-20 20:01:12 -0400

    from json

--- src/Message.h
+++ src/Message.h
@@ -1,34 +1,46 @@
-#ifndef REST_BEACON_MESSAGE_H
-#define REST_BEACON_MESSAGE_H
+#pragma once
 
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <map>
 
 struct Message {
-    std::map<String, String> properties;
+  std::map<String, String> properties;
 
-    void addProperty(const String& key, const String& value) {
-        properties[key] = value;
-    }
+  void addProperty(const String &key, const String &value) {
+    properties[key] = value;
+  }
 
-    String getProperty(const String& key) const {
-        auto it = properties.find(key);
-        return it != properties.end() ? it->second : "";
-    }
+  String getProperty(const String &key) const {
+    auto it = properties.find(key);
+    return it != properties.end() ? it->second : "";
+  }
 
-    String toJson() const {
-        String json = "{";
-        bool firstProperty = true;
-        for (const auto& pair : properties) {
-            if (!firstProperty) {
-                json += ",";
-            }
-            json += "\"" + pair.first + "\": \"" + pair.second + "\"";
-            firstProperty = false;
-        }
-        json += "}";
-        return json;
+  String toJson() const {
+    String json = "{";
+    bool firstProperty = true;
+    for (const auto &pair : properties) {
+      if (!firstProperty) {
+        json += ",";
+      }
+      json += "\"" + pair.first + "\": \"" + pair.second + "\"";
+      firstProperty = false;
     }
+    json += "}";
+    return json;
+  }
+
+  static Message fromJson(const String &jsonStr) {
+    Message msg;
+    JsonDocument doc;
+    DeserializationError err = deserializeJson(doc, jsonStr);
+    if (err) {
+      return msg;
+    }
+    for (JsonPair kv : doc.as<JsonObject>()) {
+      String key = kv.key().c_str();
+      msg.addProperty(key, kv.value().as<String>());
+    }
+    return msg;
+  }
 };
-
-#endif
